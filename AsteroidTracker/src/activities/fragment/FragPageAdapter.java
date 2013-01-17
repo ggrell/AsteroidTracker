b1d0eea9b11cbe67130dc17a997c615d6cf1ccca
David Greenfield <dgreenfield@constantcontact.com>
2013-01-16 20:24:43 -0800

    Removed tabHost from fragPager adapter, caused a NPE and not being used in replacement of actionBar tabs

--- AsteroidTracker/src/activities/fragment/FragPageAdapter.java
+++ AsteroidTracker/src/activities/fragment/FragPageAdapter.java
@@ -27,25 +27,14 @@ import android.util.Log;
 import android.widget.TabHost.TabContentFactory;
 
 public class FragPageAdapter extends FragmentPagerAdapter 
-implements TabHost.OnTabChangeListener, ViewPager.OnPageChangeListener, ActionBar.TabListener{
+implements ViewPager.OnPageChangeListener, ActionBar.TabListener{
 
     private ViewPager mViewPager;
-    private TabHost mTabHost;
     private final ArrayList mTabs = new ArrayList();
     private Context mContext = null;
     String[] tabNames = {"RECENT", "UPCOMING", "IMPACT RISK", "NEWS"};
     private ActionBar mBar;
-    
-    public FragPageAdapter(SherlockFragmentActivity activity, TabHost tabHost, ViewPager pager) {
-        super(activity.getSupportFragmentManager());
-        mContext = activity;
-        mTabHost = tabHost;
-        mViewPager = pager;
-        mTabHost.setOnTabChangedListener(this);
-        mViewPager.setAdapter(this);
-        mViewPager.setOnPageChangeListener(this);
-    }
-    
+
     public FragPageAdapter(SherlockFragmentActivity activity, ActionBar bar, ViewPager pager) {
         super(activity.getSupportFragmentManager());
         mContext = activity;
@@ -69,16 +58,6 @@ implements TabHost.OnTabChangeListener, ViewPager.OnPageChangeListener, ActionBa
     public Fragment getItem(int position) {
         TabInfo info = (TabInfo) mTabs.get(position);
         return Fragment.instantiate(mContext, info.clss.getName(), info.args);
-    }
-
-    public void addTab(TabHost.TabSpec tabSpec, Class clss, Bundle args) {
-        View tabview = createTabView(mTabHost.getContext(), tabSpec.getTag());
-        tabSpec.setContent(new TabFactory(mContext));
-        tabSpec.setIndicator(tabview);
-        TabInfo info = new TabInfo(clss, args);
-        mTabs.add(info);
-        mTabHost.addTab(tabSpec);
-        notifyDataSetChanged();
     }
 
     public void addTab(ActionBar.Tab tab, Class<? extends Fragment> clss, Bundle args) {
@@ -128,12 +107,7 @@ implements TabHost.OnTabChangeListener, ViewPager.OnPageChangeListener, ActionBa
 
     public void onPageSelected(int arg0) {}
 
-  public void onTabChanged(String tabId) {
-      int pos = mTabHost.getCurrentTab();
-      this.mViewPager.setCurrentItem(pos);
-  }
-
- public void onTabSelected(Tab tab, FragmentTransaction ft) {
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
         Object tag = tab.getTag();
         for (int i=0; i<mTabs.size(); i++) {
             if (mTabs.get(i) == tag) {
